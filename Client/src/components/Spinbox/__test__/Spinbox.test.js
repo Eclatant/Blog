16086f112820b895d304b855e2728b15088a1ee4
Jeewhan <distinct19@gmail.com>
2017-06-19 01:13:33 +0900

    TestCode Refactoring
    Describe로 공통된 코드들 묶어주기

--- Client/src/components/Spinbox/__test__/Spinbox.test.js
+++ Client/src/components/Spinbox/__test__/Spinbox.test.js
@@ -3,18 +3,18 @@ import { mount } from 'enzyme';
 
 import Spinbox from '../index';
 
-let spinbox;
-
-beforeEach(() => {
-  spinbox = mount(<Spinbox />);
-});
-
 function getValueFromTargetNode(target) {
   const targetNode = target.getDOMNode();
   return Number(targetNode.value);
 }
 
-describe('spinbox.js > ', () => {
+describe('기본값 > ', () => {
+  let spinbox;
+
+  beforeEach(() => {
+    spinbox = mount(<Spinbox />);
+  });
+
   test('스핀박스를 생성하면 기본값은 200이어야 한다.', () => {
     // given
     // when
@@ -24,12 +24,20 @@ describe('spinbox.js > ', () => {
     const actualValue = getValueFromTargetNode(input);
     expect(actualValue).toEqual(200);
   });
+});
+
+describe('값 입력 >', () => {
+  let spinbox;
+  let input;
+
+  beforeEach(() => {
+    spinbox = mount(<Spinbox />);
+    input = spinbox.find('input');
+  });
 
   test('입력 폼에 999를 입력할 수 있다.', () => {
     // given
     // when
-    const input = spinbox.find('input');
-
     input.simulate('change', {
       target: {
         value: '999',
@@ -44,8 +52,6 @@ describe('spinbox.js > ', () => {
   test('입력 폼에 0을 입력할 수 있다.', () => {
     // given
     // when
-    const input = spinbox.find('input');
-
     input.simulate('change', {
       target: {
         value: '0',
@@ -56,32 +62,39 @@ describe('spinbox.js > ', () => {
     const actualValue = getValueFromTargetNode(input);
     expect(actualValue).toEqual(0);
   });
+});
+
+describe('값 증가 >', () => {
+  const defaultValue = 200;
+  let spinbox;
+  let input;
+  let incrementBtn;
+
+  beforeEach(() => {
+    spinbox = mount(<Spinbox />);
+    input = spinbox.find('input');
+    incrementBtn = spinbox.find('[data-name="increment"]');
+  });
 
   test('증가 버튼을 클릭하여 값을 1 증가시킬 수 있다', () => {
     // given
-    const defaultValue = 200;
-
     // when
-    const incrementBtn = spinbox.find('[data-name="increment"]');
     incrementBtn.simulate('click');
 
     // then
-    const actualValue = getValueFromTargetNode(spinbox.find('input'));
+    const actualValue = getValueFromTargetNode(input);
     expect(actualValue).toEqual(defaultValue + 1);
   });
 
   test('증가 버튼을 세 번 클릭하여 값을 3 증가시킬 수 있다', () => {
     // given
-    const defaultValue = 200;
-
     // when
-    const incrementBtn = spinbox.find('[data-name="increment"]');
     incrementBtn.simulate('click');
     incrementBtn.simulate('click');
     incrementBtn.simulate('click');
 
     // then
-    const actualValue = getValueFromTargetNode(spinbox.find('input'));
+    const actualValue = getValueFromTargetNode(input);
     expect(actualValue).toEqual(defaultValue + 3);
   });
 });

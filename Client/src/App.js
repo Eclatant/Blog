06e68770ee4876c38a4fed0a1cc12e32478105e0
Jeewhan <distinct19@gmail.com>
2017-06-18 17:13:05 +0900

    #3 Route Component
    Home, About, Posts, NotFound

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import Header from './components/Header';
+
+import Home from './routes/Home';
+import About from './routes/About';
+import Posts from './routes/Posts';
+import NotFound from './routes/NotFound';
+
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/posts" component={Posts} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;

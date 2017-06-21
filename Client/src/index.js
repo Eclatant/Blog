5dfa8f4a64a491caf18953089fc52d43e8fa134a
Jeewhan <distinct19@gmail.com>
2017-06-21 20:04:54 +0900

    CSS 수정

--- Client/src/index.js
+++ Client/src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './App';
+import './index.css';
 
 import ToDoApp from './pages/ToDo/';
 import Spinbox from './components/Spinbox/';
 
-// ReactDOM.render(<Spinbox />, document.getElementById('root'));
-// ReactDOM.render(<ToDoApp />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- Client/src/pages/NotFound.js
+++ Client/src/pages/NotFound.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const NotFound = () => (
-  <div>
-    404
+  <div className="page-404">
+    <img src="https://commastitcher.files.wordpress.com/2014/06/github404.png" />
   </div>
 );
 

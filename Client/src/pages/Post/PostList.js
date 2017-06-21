b6b65f9a08520cbe5035f1201fa93d930ca8ea40
Jeewhan <distinct19@gmail.com>
2017-06-21 20:22:51 +0900

    Post 관련 디렉토리 구조 변경 및 Router 수정

---
+++ Client/src/pages/Post/PostList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+
+import PostDetail from './PostDetail';
+
+export default class PostList extends Component {
+  render() {
+    return (
+      <div className="page-post">
+        <h1>Post</h1>
+        <Link to="/posts/:id">title</Link>
+        <Route path="/posts/:title" component={PostDetail} />
+      </div>
+      );
+  }
+}
+

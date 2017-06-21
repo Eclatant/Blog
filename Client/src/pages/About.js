7ab10ae3a28408220ef6d675a13f0777fc69da77
Jeewhan <distinct19@gmail.com>
2017-06-21 20:20:43 +0900

    About 내용 추가

--- Client/src/pages/About.js
+++ Client/src/pages/About.js
@@ -1,8 +1,48 @@
 import React from 'react';
 
 const About = () => (
-  <div>
-    About
+  <div className="page-about">
+    <h1>About Me</h1>
+
+    <br />
+
+    <h2>자기 소개</h2>
+    <p>
+      안녕하세요, 류지환입니다.<br />
+      일을 할 때는 불가능한 최고가 아니라, 가능한 최선을 찾으려고 합니다.<br />
+      실력을 쌓고, 돈을 위해 일하는 것이 아니라 계속 일을 할 수 있도록 돈을 벌어야 하는 시기라고 생각합니다.<br />
+      컴퓨터는 표현의 수단이라고 생각하며, 그 너머에 있는 사람이 중요하다고 생각합니다.<br />
+      2017년에 처음으로 시작한 자바스크립트를 재미있어 하고 관심을 쏟고 있습니다.<br />
+      다양하고 자유로운 개발 방식보다는 PEP8, CoC, Redux 등의 일관되고 명시적인 개발 방식을 보다 선호합니다.<br />
+    </p>
+
+    <br />
+
+    <h2>직무 경험</h2>
+    <ul>
+      <li>재수학원 부원장 (2012/12 ~ 2015/4)</li>
+      <li>패스트캠퍼스 Senior Operation Manager (2015/5 ~ 2016/4)</li>
+    </ul>
+
+    <br />
+
+    <h2>프로그래밍 관련 교육</h2>
+    <ul>
+      <li>패스트캠퍼스 웹 프로그래밍 스쿨 <a href="http://school.fastcampus.co.kr/dev_wps/">링크</a></li>
+      <li>코드 스쿼드 웹 프론트엔드 화이트 레벨 <a href="http://codesquad.kr/html/program/dist/masters.html">링크</a></li>
+      <li>코드 스쿼드 웹 프론트엔드 블루 레벨 <a href="http://codesquad.kr/html/program/dist/masters.html">링크</a></li>
+      <li>한국방송통신대학교 컴퓨터과학과 재학중 <a href="http://cs1.knou.ac.kr/">링크</a></li>
+    </ul>
+
+    <br />
+
+    <h2>프로젝트 경험</h2>
+    <ul>
+      <li>트레바리 홈페이지 <a href="http://trevari.co.kr">링크</a></li>
+      <li>규모: 회원 1000 ~ 1500명</li>
+      <li>개발 항목: 기존 개발 사항 유지보수 및 관리자 기능 개발</li>
+    </ul>
+
   </div>
 );
 

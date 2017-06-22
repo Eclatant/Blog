e41ae067aa6fa95bf63b03ad78ed6357fabcfc0a
Jeewhan <distinct19@gmail.com>
2017-06-22 21:43:13 +0900

    #7 Post관련 Router와 Header 수정

--- Client/src/share/Header/index.js
+++ Client/src/share/Header/index.js
@@ -5,7 +5,7 @@ import './Header.css';
 const Header = () => (
   <div className="header">
     <NavLink exact to="/" className="item" activeClassName="active">About</NavLink>
-    <NavLink to="/post" className="item" activeClassName="active">Posts</NavLink>
+    {/* <NavLink to="/post" className="item" activeClassName="active">Posts</NavLink> */}
     <NavLink to="/todo" className="item" activeClassName="active">ToDo</NavLink>
   </div>
 );

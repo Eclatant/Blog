f7b0b4eccf14a4bafbc8283ae8e5fa0bdb84155a
Jeewhan <distinct19@gmail.com>
2017-06-22 21:37:18 +0900

    Post Page Skeleton

--- Client/src/pages/Post/PostList.js
+++ Client/src/pages/Post/PostList.js
@@ -6,7 +6,7 @@ import PostDetail from './PostDetail';
 export default class PostList extends Component {
   render() {
     return (
-      <div className="page-post">
+      <div className="page-post post-list">
         <h1>Post</h1>
         <Link to="/posts/:id">title</Link>
         <Route path="/posts/:title" component={PostDetail} />
